449e516048ea49eda07a3694ce76f8fede04c4fd
Sevann Triztan <striztan@dacgroup.com>
2022-07-20 13:46:00 -0300

    second commit

---
+++ MicroservicesRabbitMQ/MicroservicesRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroservicesRabbitMQ.Domain.Core.Bus;
+using MicroservicesRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroservicesRabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+            services.Configure<RabbitMQSettings>(c => configuration.GetSection("RabbitMQSettings"));
+        }
+    }
+}
